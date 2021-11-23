0d13ad4b812850b72fc4325595d8f6b92049a328
Bondidos <gglbond@gmail.com>
2021-11-23 20:16:32 +0300

    Example_done_with_success

--- app/src/main/java/com/example/wot_login_test/MainActivity.kt
+++ app/src/main/java/com/example/wot_login_test/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 
+private const val TOKEN = " token"
 private const val URL_REQUEST = "request"
 
 class MainActivity : AppCompatActivity() {
@@ -24,11 +25,17 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this,WebViewActivity::class.java).apply {
             putExtra(
                 URL_REQUEST,
-                "https://api.worldoftanks.eu/wot/auth/login/?application_id=5d489c586717c2b76ade8bea16607167&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F"
+               // "https://api.worldoftanks.ru/wot/auth/login/?application_id=5d489c586717c2b76ade8bea16607167&display=page&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F"
+             "https://api.worldoftanks.eu/wot/auth/login/?application_id=5d489c586717c2b76ade8bea16607167&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F"
             )
         }
         val launch =registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            it?.let{ result ->
+                if(result.resultCode == 1){
+                    textView.text = result.data?.extras?.getString(TOKEN)
+                }
 
+            }
         }
 
         button.setOnClickListener {
--- app/src/main/java/com/example/wot_login_test/WebViewActivity.kt
+++ app/src/main/java/com/example/wot_login_test/WebViewActivity.kt
@@ -1,12 +1,17 @@
 
 package com.example.wot_login_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 private const val URL_REQUEST = "request"
+private const val REDIRECT_URI = "https://developers.wargaming.net/reference/all/wot/auth/login/"
+private const val TOKEN = " token"
 
 class WebViewActivity : AppCompatActivity() {
 
@@ -18,12 +23,47 @@ class WebViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_web_view)
 
         browser = findViewById(R.id.webView)
+        browser.webViewClient = AuthWebViewClient()
+
+
+        val webSettings = browser.settings
+        true.also { webSettings.javaScriptEnabled = it }
+        webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
+
+
         intent.extras?.let {
             url = it.getString(URL_REQUEST)
-            Log.d("Log",url ?: "error")
+            Log.d("Log", url ?: "error")
             browser.loadUrl(url ?: "https://www.google.com/")
-
+            //browser.loadUrl("https://api.worldoftanks.ru/wot/auth/login/?application_id=5d489c586717c2b76ade8bea16607167&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F&display=popup")
         }
 
+    }
+
+    fun returnResult(string: String){
+        val intent = Intent(this,MainActivity::class.java).apply {
+            this.putExtra(
+                TOKEN,string)
+        }
+        setResult(1,intent)
+        finish()
+    }
+
+    inner class AuthWebViewClient : WebViewClient(){
+
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+
+            // если URL содержит REDIRECT_URI, значит это нам пришел код
+            // и WebView не надо открывать этот URL
+
+            if (url?.contains(REDIRECT_URI, true)!!) {
+                // парсим URL, чтобы извлечь из него код
+
+                Log.d("Log", url)
+                returnResult(url)
+                return true
+            }
+            return false
+        }
     }
 }
